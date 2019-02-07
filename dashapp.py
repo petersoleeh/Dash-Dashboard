f2d3dabcfbac36a5692a7cc4f9a9cfa46ad8e935
peter soleeh <petersoleeh@gmail.com>
2019-02-07 12:17:56 +0300

    mvp

--- dashapp.py
+++ dashapp.py
@@ -20,7 +20,7 @@ dashapp = dash.Dash(__name__, server=flask_app, sharing=True, external_styleshee
 
 
 dashapp.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Dashboard 1'),
 
     html.Div(children='''
         Dash: A web application framework for Python.
@@ -43,24 +43,39 @@ dashapp.layout = html.Div(children=[
 
 dashapp = dash.Dash(__name__, server=flask_app, sharing=True, external_stylesheets=external_stylesheets, url_base_pathname='/dashboard1/', csrf_protect=False)
 
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
 
-dashapp.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python2.
-    '''),
-
+dashapp.layout = html.Div([
+    html.H1(children='Dashboard 2'),
     dcc.Graph(
-        id='example-graph',
+        id='life-exp-vs-gdp',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 4, 3], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 1, 5], 'type': 'bar', 'name': u'Montréal'},
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     )
 ])
